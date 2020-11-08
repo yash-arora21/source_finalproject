f265eb2cce3ed3ad69faa3540b8c38d61767f056
yash-arora21 <yra4ah@virginia.edu>
2020-11-08 14:36:47 -0500

    commiting simple map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from "react"; //start by finding a react wrapper for the JS stuff on mapbox
+import ReactMapGL from "react-map-gl";
 function App() {
+  const [viewport, setViewport] = useState({
+    width: 800,
+    height: 600,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 3,
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactMapGL
+    mapboxApiAccessToken = {process.env.REACT_APP_MAPBOX_TOKEN}
+      {...viewport}
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+    >
+    </ReactMapGL>
   );
 }
 
