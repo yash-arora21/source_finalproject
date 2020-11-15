349def41814da131c89636aee41b7a1446e68d99
yash-arora21 <yra4ah@virginia.edu>
2020-11-15 11:30:44 -0500

    added popup, needs functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React, {useState} from "react"; //start by finding a react wrapper for the JS stuff on mapbox
-import ReactMapGL from "react-map-gl";
+import React, {useEffect, useState, Component} from "react"; //start by finding a react wrapper for the JS stuff on mapbox
+import ReactMapGL, {Popup} from "react-map-gl";
+import axios from 'axios';
+//import data from "./data.geojson";
 function App() {
+  const url = 'https://api.covid19api.com/summary';
+  //const [popup, setPopup] = useState(true);
+
+  /*axios.get("./data.geojson")
+    .then(response => {
+      response.data.Countries.map(place=> ({
+        country: '${place.Country}',
+        cases: '${place.TotalConfirmed}',
+        deaths: '${place.TotalDeaths}'
+      }))
+    });*/
+  
   const [viewport, setViewport] = useState({
-    width: 800,
-    height: 600,
+    width: 1300,
+    height: 900,
     latitude: 37.7577,
     longitude: -122.4376,
     zoom: 3,
@@ -14,6 +28,13 @@ function App() {
       {...viewport}
       onViewportChange={nextViewport => setViewport(nextViewport)}
     >
+      <Popup
+        longitude={-122.43}
+        latitude={37.75}
+        closeButton={false}
+        closeOnClick={false}>
+        Popup Test
+      </Popup>
     </ReactMapGL>
   );
 }
